5994b6fd9918d27f0243e363d5feef235b5415bb
prest <prest@DESKTOP-9VPL2VA>
2022-10-11 16:38:03 +0300

    test commit

--- laba1/src/laba1/Test.java
+++ laba1/src/laba1/Test.java
@@ -4,6 +4,6 @@ public class Test
 {  
     public static void main(String args[])  
     {  
-        System.out.print("first statement. ");  
+        System.out.print("second statement. ");  
     }  
 } 
