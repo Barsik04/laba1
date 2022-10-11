39464e1bf529d42de36a61a95638f932ce314643
prest <prest@DESKTOP-9VPL2VA>
2022-10-11 17:30:57 +0300

    v0.2

---
+++ laba1/src/laba1/Employees.java
@@ -0,0 +1,32 @@
+package laba1;
+
+public class Employees {
+	private String firstName; 
+	private String lastName; 
+	private String location;  
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}   
+}
